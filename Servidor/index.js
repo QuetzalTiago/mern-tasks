c1e182e56d34857ab6ca576c3b9b2ae6dede7cc9
Quetzal Tiago Romero Iaurreche <tiago.romero@globant.com>
2021-06-15 10:25:08 -0300

    added mongodb

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const conectarDB = require("./config/db");
+
+//crear el servidor
+const app = express();
+
+//conectar a la db
+conectarDB();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//definiar la pag principal
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+//arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
